c7ad1f2cb73ab1b01fad0a988158393d240e7ef5
Pankaj Kumar <pankajallen000@gmail.com>
2025-03-06 21:40:35 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -15,11 +15,12 @@ await connectDB();
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = [
-    'http://localhost:5173',  // For local development
-    'https://incruiter-auth-gkku.vercel.app'  // Your Vercel frontend URL
-  ];
-app.use(cors({origin: allowedOrigins, credentials: true})); 
+// const allowedOrigins = [
+//     'http://localhost:5173',  
+//     'https://incruiter-auth-gkku.vercel.app'  
+//   ];
+
+app.use(cors()); 
 
 // api endpoints
 app.get('/', (req, res) => {
