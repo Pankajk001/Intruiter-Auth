9fd387284e0310f6556746f090d724dba75bfc9c
Pankaj Kumar <pankajallen000@gmail.com>
2025-03-06 23:01:45 +0530

    updated header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -15,8 +15,6 @@ const Header = () => {
 
       <h2 className='text-3xl sm:text-5xl font-semibold mb-4 text-gray-300'>Backend Assignment Solution</h2>
       <p className='mb-8 max-w-md text-gray-400'>My name is <span className='font-bold'>Pankaj Kumar</span> and this is the assignment that was give by the Incruiter team to test my skills. This is the solution of the <span className='font-bold'>Backend Assignment.</span></p>
-
-      <br />
       <span className='text-gray-300 font-bold'>Go to Login Button and See the Magic. 😍</span>
 
       <button className='text-gray-300 hover:text-gray-900 font-semibold border border-gray-100 rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all'>Get Started</button>
